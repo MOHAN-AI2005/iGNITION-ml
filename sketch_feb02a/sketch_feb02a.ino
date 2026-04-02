0e2ab944fb0e9fc05873a489dc04e4a2716e44d9
Sairam <Sairam@localhost.localdomain>
2026-04-02 22:51:15 +0530

    Added sketch_feb02a-mpu able to send data to server

---
+++ sketch_feb02a/sketch_feb02a.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22);  // SDA, SCL
+
+  mpu.initialize();
+
+  if (mpu.testConnection()) {
+    Serial.println("MPU6050 Connected");
+  } else {
+    Serial.println("MPU6050 Connection Failed");
+  }
+}
+
+void loop() {
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  // Send as CSV (easy for Python)
+  Serial.print(ax); Serial.print(",");
+  Serial.print(ay); Serial.print(",");
+  Serial.print(az); Serial.print(",");
+  Serial.print(gx); Serial.print(",");
+  Serial.print(gy); Serial.print(",");
+  Serial.println(gz);
+
+  delay(200);
+}
